b744192ab324cb939a0c804a1486d22c5e5f2344
WalterNights <walternightsgmail.com>
2023-03-16 15:16:27 -0500

    build divisions into the project, assign and reorganization the elements, styles and scripts and change class name for a best optimization

--- navigation.js
+++
@@ -1,9 +0,0 @@
-const menuButton = document.querySelector('.menu-button');
-const dropdownContent = document.querySelector('.dropdown-content');
-const menuContainer = document.querySelector('.menu-container');
-
-menuButton.addEventListener('click', () => {
-  menuContainer.classList.toggle('hidden');
-  var menuList = document.querySelector(".menu-list");
-  menuList.classList.toggle("display-block");
-});
